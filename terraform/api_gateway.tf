b9ba4b78f24de3b1975017c67acf4c8b191087c7
BrunoGino <bruno.gino24@gmail.com>
2024-12-30 21:03:09 -0300

    feat: uncommenting api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,47 +1,51 @@
-# module "api_gateway" {
-#   source = "terraform-aws-modules/apigateway-v2/aws"
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
 
-#   name          = "iamind-http"
-#   description   = "IAMind API Gateway"
-#   protocol_type = "HTTP"
+  name          = "iamind-http"
+  description   = "IAMind API Gateway"
+  protocol_type = "HTTP"
 
-#   domain_name = "api.iamind.com"
+  hosted_zone_name      = "api.iamind.com"
+  domain_name           = "api.iamind.com"
+  create_domain_records = true
+  create_domain_name    = true
+  create_certificate    = true
 
-#   cors_configuration = {
-#     allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-#     allow_methods = ["*"]
-#     allow_origins = ["*"]
-#   }
+  cors_configuration = {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
 
-#   # Access logs
-#   stage_access_log_settings = {
-#     create_log_group            = true
-#     log_group_retention_in_days = 7
-#     format = jsonencode({
-#       context = {
-#         domainName              = "$context.domainName"
-#         integrationErrorMessage = "$context.integrationErrorMessage"
-#         protocol                = "$context.protocol"
-#         requestId               = "$context.requestId"
-#         requestTime             = "$context.requestTime"
-#         responseLength          = "$context.responseLength"
-#         routeKey                = "$context.routeKey"
-#         stage                   = "$context.stage"
-#         status                  = "$context.status"
-#         error = {
-#           message      = "$context.error.message"
-#           responseType = "$context.error.responseType"
-#         }
-#         identity = {
-#           sourceIP = "$context.identity.sourceIp"
-#         }
-#         integration = {
-#           error             = "$context.integration.error"
-#           integrationStatus = "$context.integration.integrationStatus"
-#         }
-#       }
-#     })
-#   }
+  # Access logs
+  stage_access_log_settings = {
+    create_log_group            = true
+    log_group_retention_in_days = 7
+    format = jsonencode({
+      context = {
+        domainName              = "$context.domainName"
+        integrationErrorMessage = "$context.integrationErrorMessage"
+        protocol                = "$context.protocol"
+        requestId               = "$context.requestId"
+        requestTime             = "$context.requestTime"
+        responseLength          = "$context.responseLength"
+        routeKey                = "$context.routeKey"
+        stage                   = "$context.stage"
+        status                  = "$context.status"
+        error = {
+          message      = "$context.error.message"
+          responseType = "$context.error.responseType"
+        }
+        identity = {
+          sourceIP = "$context.identity.sourceIp"
+        }
+        integration = {
+          error             = "$context.integration.error"
+          integrationStatus = "$context.integration.integrationStatus"
+        }
+      }
+    })
+  }
 
-#   tags = local.default_tags
-# }
+  tags = local.default_tags
+}
