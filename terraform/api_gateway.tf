0aa3d7e656b7398dcaa55e3bf60cc1b3b57f98c2
BrunoGino <bruno.gino24@gmail.com>
2024-12-31 14:35:11 -0300

    fix: removing route53 props from api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,51 +1,52 @@
 module "api_gateway" {
-  source = "terraform-aws-modules/apigateway-v2/aws"
+  source  = "terraform-aws-modules/apigateway-v2/aws"
+  version = "5.2.1"
 
+  create        = true
   name          = "iamind-http"
   description   = "IAMind API Gateway"
   protocol_type = "HTTP"
 
-  hosted_zone_name      = "api.iamind.com"
-  domain_name           = "api.iamind.com"
-  create_domain_records = true
-  create_domain_name    = true
-  create_certificate    = true
+  domain_name                  = "api.iamind.com"
+  create_domain_records        = true
+  create_certificate           = true
+  create_domain_name           = true
+  disable_execute_api_endpoint = false
 
-  cors_configuration = {
-    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-    allow_methods = ["*"]
-    allow_origins = ["*"]
-  }
+  # cors_configuration = {
+  #   allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+  #   allow_methods = ["*"]
+  #   allow_origins = ["*"]
+  # }
 
-  # Access logs
-  stage_access_log_settings = {
-    create_log_group            = true
-    log_group_retention_in_days = 7
-    format = jsonencode({
-      context = {
-        domainName              = "$context.domainName"
-        integrationErrorMessage = "$context.integrationErrorMessage"
-        protocol                = "$context.protocol"
-        requestId               = "$context.requestId"
-        requestTime             = "$context.requestTime"
-        responseLength          = "$context.responseLength"
-        routeKey                = "$context.routeKey"
-        stage                   = "$context.stage"
-        status                  = "$context.status"
-        error = {
-          message      = "$context.error.message"
-          responseType = "$context.error.responseType"
-        }
-        identity = {
-          sourceIP = "$context.identity.sourceIp"
-        }
-        integration = {
-          error             = "$context.integration.error"
-          integrationStatus = "$context.integration.integrationStatus"
-        }
-      }
-    })
-  }
+  # stage_access_log_settings = {
+  #   create_log_group            = true
+  #   log_group_retention_in_days = 7
+  #   format = jsonencode({
+  #     context = {
+  #       domainName              = "$context.domainName"
+  #       integrationErrorMessage = "$context.integrationErrorMessage"
+  #       protocol                = "$context.protocol"
+  #       requestId               = "$context.requestId"
+  #       requestTime             = "$context.requestTime"
+  #       responseLength          = "$context.responseLength"
+  #       routeKey                = "$context.routeKey"
+  #       stage                   = "$context.stage"
+  #       status                  = "$context.status"
+  #       error = {
+  #         message      = "$context.error.message"
+  #         responseType = "$context.error.responseType"
+  #       }
+  #       identity = {
+  #         sourceIP = "$context.identity.sourceIp"
+  #       }
+  #       integration = {
+  #         error             = "$context.integration.error"
+  #         integrationStatus = "$context.integration.integrationStatus"
+  #       }
+  #     }
+  #   })
+  # }
 
   tags = local.default_tags
 }
