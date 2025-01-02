e3ad5f26191ae4a932703668b41bf94875732ec4
BrunoGino <bruno.gino24@gmail.com>
2025-01-02 16:45:57 -0300

    fix: uncommenting api-gw

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,14 +1,14 @@
-# resource "aws_apigatewayv2_api" "iamind_api_gateway" {
-#   name          = "iamind_api_gateway"
-#   protocol_type = "HTTP"
+resource "aws_apigatewayv2_api" "iamind_api_gateway" {
+  name          = "iamind_api_gateway"
+  protocol_type = "HTTP"
 
-#   cors_configuration {
-#     allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-#     allow_methods = ["*"]
-#     allow_origins = ["*"]
-#   }
+  cors_configuration {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
 
-#   fail_on_warnings = true
+  fail_on_warnings = true
 
-#   tags = local.default_tags
-# }
+  tags = local.default_tags
+}
