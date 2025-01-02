3c7fcab54878798abf7352372ecf12d4dc4b099a
BrunoGino <bruno.gino24@gmail.com>
2025-01-02 15:31:17 -0300

    refactor: removing api-gateway module

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,52 +1,14 @@
-module "api_gateway" {
-  source  = "terraform-aws-modules/apigateway-v2/aws"
-  version = "5.2.1"
-
-  create        = true
-  name          = "iamind-http"
-  description   = "IAMind API Gateway"
+resource "aws_apigatewayv2_api" "iamind_api_gateway" {
+  name          = "iamind_api_gateway"
   protocol_type = "HTTP"
 
-  domain_name                  = "api.iamind.com"
-  create_domain_records        = true
-  create_certificate           = true
-  create_domain_name           = true
-  disable_execute_api_endpoint = false
+  cors_configuration {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
 
-  # cors_configuration = {
-  #   allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-  #   allow_methods = ["*"]
-  #   allow_origins = ["*"]
-  # }
-
-  # stage_access_log_settings = {
-  #   create_log_group            = true
-  #   log_group_retention_in_days = 7
-  #   format = jsonencode({
-  #     context = {
-  #       domainName              = "$context.domainName"
-  #       integrationErrorMessage = "$context.integrationErrorMessage"
-  #       protocol                = "$context.protocol"
-  #       requestId               = "$context.requestId"
-  #       requestTime             = "$context.requestTime"
-  #       responseLength          = "$context.responseLength"
-  #       routeKey                = "$context.routeKey"
-  #       stage                   = "$context.stage"
-  #       status                  = "$context.status"
-  #       error = {
-  #         message      = "$context.error.message"
-  #         responseType = "$context.error.responseType"
-  #       }
-  #       identity = {
-  #         sourceIP = "$context.identity.sourceIp"
-  #       }
-  #       integration = {
-  #         error             = "$context.integration.error"
-  #         integrationStatus = "$context.integration.integrationStatus"
-  #       }
-  #     }
-  #   })
-  # }
+  fail_on_warnings = true
 
   tags = local.default_tags
 }
