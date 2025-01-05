e96b0340b954078e31f55c5d668206adebbd9226
BrunoGino <bruno.gino24@gmail.com>
2025-01-05 14:28:27 -0300

    feat: creating alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,16 +1,37 @@
-# module "alb" {
-#   source              = "terraform-aws-modules/alb/aws"
-#   version             = "2.4.0"
-#   alb_name            = "iamind-alb"
-#   alb_protocols       = ["HTTPS"]
-#   alb_security_groups = [aws_security_group.iamind_sg_tls_http.id]
-#   certificate_arn     = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-#   create_log_bucket   = true
-#   enable_logging      = true
-#   health_check_path   = "/"
-#   log_bucket_name     = "logs-us-east-2-123456789012"
-#   log_location_prefix = "iamind-alb"
-#   subnets             = ["subnet-abcde012", "subnet-bcde012a"]
-#   tags                = merge(local.default_tags, { Name = "iamind-alb" })
-#   vpc_id              = aws_vpc.iamind_vpc.id
-# }
+resource "aws_lb" "iamind_alb" {
+  name               = "iamind_alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.iamind_sg_tls_http.id]
+  subnets            = [aws_subnet.iamind_subnet_public1.id, aws_subnet.iamind_subnet_public2.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.iamind_access_logs_bucket.id
+    prefix  = "iamind_alb"
+    enabled = true
+  }
+
+  tags = local.default_tags
+}
+
+resource "aws_lb_target_group" "iamind_alb_tg_https" {
+  name     = "alb_tg_https"
+  port     = 443
+  protocol = "TCP"
+  vpc_id   = aws_vpc.iamind_vpc.id
+}
+
+resource "aws_lb_listener" "iamind_alb_listener_https" {
+  load_balancer_arn = aws_lb.iamind_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.iamind_certificate.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.iamind_alb_tg_https.arn
+  }
+}
