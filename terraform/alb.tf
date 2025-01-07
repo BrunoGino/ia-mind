2788b7107e3b93e09fe8c65358cf635049931c99
BrunoGino <bruno.gino24@gmail.com>
2025-01-07 20:03:46 -0300

    fix: switching alb to http for now

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -18,7 +18,7 @@ resource "aws_lb" "iamind_alb" {
 
 resource "aws_lb_target_group" "iamind_alb_tg_https" {
   name     = "iamind-alb-tg-https"
-  port     = 443
+  port     = 80
   protocol = "TCP"
   vpc_id   = aws_vpc.iamind_vpc.id
 }
@@ -26,9 +26,9 @@ resource "aws_lb_target_group" "iamind_alb_tg_https" {
 resource "aws_lb_listener" "iamind_alb_listener_https" {
   load_balancer_arn = aws_lb.iamind_alb.arn
   port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.iamind_certificate.arn
+  protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = aws_acm_certificate.iamind_certificate.arn
 
   default_action {
     type             = "forward"
