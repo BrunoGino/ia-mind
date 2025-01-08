aa390394b3d672ed718e51fd6d1db8f5e0464ae0
BrunoGino <bruno.gino24@gmail.com>
2025-01-07 22:14:08 -0300

    refactor: bye obscure ecs module

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,74 +1,72 @@
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
+resource "aws_ecs_cluster" "iamind_ecs_cluster" {
+  name = "iamind_ecs_cluster"
 
-  cluster_name = "iamind-cluster"
-
-  fargate_capacity_providers = {
-    FARGATE = {
-      default_capacity_provider_strategy = {
-        weight = 10
-      }
-    }
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 90
-      }
-    }
-  }
-
-  services = {
-    session-management = {
-      cpu    = 1024
-      memory = 4096
-
-      container_definitions = {
-        session-management = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = "brunogino/iamind-session_management:latest"
-          port_mappings = [
-            {
-              name          = "session-management"
-              containerPort = 5000
-              protocol      = "tcp"
-            }
-          ]
-          readonly_root_filesystem = false
-
-          enable_cloudwatch_logging = true
-          memory_reservation        = 100
-        }
-      }
-
-      load_balancer = {
-        service = {
-          target_group_arn = aws_lb_target_group.iamind_alb_tg_https.arn
-          container_name   = "session-management"
-          container_port   = 5000
-        }
-      }
-
-      subnet_ids = [aws_subnet.iamind_subnet_public1.id, aws_subnet.iamind_subnet_public2.id]
-      security_group_rules = {
-        alb_ingress_3000 = {
-          type                     = "ingress"
-          from_port                = 80
-          to_port                  = 5000
-          protocol                 = "tcp"
-          description              = "Service port"
-          source_security_group_id = aws_subnet.iamind_subnet_public1.id
-        }
-        egress_all = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
-    }
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
   }
 
   tags = local.default_tags
+}
+
+resource "aws_ecs_cluster_capacity_providers" "iamind_capacity_provider" {
+  cluster_name = aws_ecs_cluster.iamind_ecs_cluster.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+
+  default_capacity_provider_strategy {
+    weight            = 80
+    base              = 1
+    capacity_provider = "FARGATE_SPOT"
+  }
+
+  default_capacity_provider_strategy {
+    weight            = 20
+    capacity_provider = "FARGATE"
+  }
+}
+
+data "template_file" "session_management_container_definitions" {
+  template = file("task-definitions/task-template.json")
+
+  vars = {
+    name  = "session_management"
+    image = "brunogino/iamind-session_management:latest"
+  }
+}
+
+resource "aws_ecs_task_definition" "session_management_task_definition" {
+  family                   = "session_management_task"
+  container_definitions    = data.template_file.session_management_container_definitions.rendered
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.deployment_role.arn
+  task_role_arn            = aws_iam_role.ecs_tasks_role.arn
+  memory                   = 512
+  cpu                      = 256
+  tags                     = local.default_tags
+
+}
+
+resource "aws_ecs_service" "session_management_service" {
+  name            = "session_management_service"
+  cluster         = aws_ecs_cluster.iamind_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.session_management_task_definition.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    security_groups = [aws_security_group.iamind_sg_tls_http.id]
+    subnets         = [aws_subnet.iamind_subnet_public1.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.iamind_alb_tg_https.arn
+    container_name   = "session_management_task"
+    container_port   = 8080
+  }
+
+  tags = local.default_tags
+
 }
