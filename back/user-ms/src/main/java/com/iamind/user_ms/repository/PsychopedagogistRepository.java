2aba4cf98c899bbe12c697e8252b8f6f0f6a1275
Gustavo Borges Guimarães <gustavo.borges.guimaraes100@gmail.com>
2025-01-08 23:38:21 -0300

    fix: removed unecessary user service/controller/dtos and added missing repository annotation to repositories

--- back/user-ms/src/main/java/com/iamind/user_ms/repository/PsychopedagogistRepository.java
+++ back/user-ms/src/main/java/com/iamind/user_ms/repository/PsychopedagogistRepository.java
@@ -2,6 +2,8 @@ package com.iamind.user_ms.repository;
 
 import com.iamind.user_ms.model.Psychopedagogist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PsychopedagogistRepository extends JpaRepository<Psychopedagogist, Long> {
 }
