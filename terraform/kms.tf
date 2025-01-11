6b134e4ebc59a57e570c530947decbb5859f295d
BrunoGino <bruno.gino24@gmail.com>
2025-01-11 00:00:14 -0300

    feat: creating kms and secret

---
+++ terraform/kms.tf
@@ -0,0 +1,35 @@
+resource "aws_kms_key" "iamind_kms_key" {
+  description             = "IAMind kms key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  tags                    = local.default_tags
+  policy                  = <<POLICY
+    {
+        "Sid": "GrantAccessToRoles",
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": [
+                "arn:aws:iam::108782061116:role/aws_gino_sol_deployment",
+                "arn:aws:iam::108782061116:role/aws_gino_sol_iamind_ecs_tasks"
+            ]
+        },
+        "Action": [
+            "kms:Decrypt",
+            "kms:Describe*",
+            "kms:Encrypt",
+            "kms:Get*",
+            "kms:GenerateDataKey*",
+            "kms:List*",
+            "kms:Update*",
+            "kms:ReEncrypt",
+            "kms:*Resource",        
+            "kms:*Alias",
+            "kms:PutKeyPolicy",
+            "kms:ScheduleKeyDeletion",
+            "kms:*Grant",
+            "kms:EnableKeyRotation"
+        ],
+        "Resource": "arn:aws:kms:eu-west-1:108782061116:key/*"
+    }
+  POLICY
+}
