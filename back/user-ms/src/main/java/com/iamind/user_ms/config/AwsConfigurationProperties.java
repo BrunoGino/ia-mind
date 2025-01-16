7c1aa6ed8fb2dfdcdba7031f6fd122544633ce80
BrunoGino <bruno.gino24@gmail.com>
2025-01-15 21:23:16 -0300

    fix: removing notblank from config class

--- back/user-ms/src/main/java/com/iamind/user_ms/config/AwsConfigurationProperties.java
+++ back/user-ms/src/main/java/com/iamind/user_ms/config/AwsConfigurationProperties.java
@@ -12,13 +12,10 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "aws")
 public class AwsConfigurationProperties {
 
-    @NotBlank(message = "AWS access key must be configured")
     private String accessKey;
 
-    @NotBlank(message = "AWS secret access key must be configured")
     private String secretAccessKey;
 
-    @NotBlank(message = "AWS region must be configured")
     private String region;
 
     private String endpoint;
