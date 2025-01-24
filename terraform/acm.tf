f933c05e8e74387d33f00615543e56d40c9a1d84
BrunoGino <bruno.gino24@gmail.com>
2025-01-23 21:26:35 -0300

    fix: fixing acm reference

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,5 +1,5 @@
 import {
-  to = aws_acm_certificate.cert
+  to = aws_acm_certificate.iamind_certificate
   id = "arn:aws:acm:eu-west-1:108782061116:certificate/d20d2a02-ad75-43d1-91c5-8573047c5856"
 }
 
